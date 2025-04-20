b9c518105992022351472e943c1dc4ba100a9c0f
Mobin <Mobinmojiri1234@gmail.com>
2025-04-20 17:58:12 +0330

    coding Abstract Device

--- src/Management/Device.java
+++ src/Management/Device.java
@@ -1,5 +1,14 @@
 package Management;
 
 public abstract class Device {
+    protected String name;
+    protected String protocol;
 
+    public Device(String name, String protocol){
+        this.name = name;
+        this.protocol = protocol;
+    }
+
+    public abstract void setProperty(String property, String value);
+    public abstract String getStatus();
 }
