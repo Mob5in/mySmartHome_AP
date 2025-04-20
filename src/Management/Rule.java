09b5845ef11eb63711f5a8a550a077f7870823a4
Mobin <Mobinmojiri1234@gmail.com>
2025-04-20 18:19:29 +0330

    coding rule

---
+++ src/Management/Rule.java
@@ -0,0 +1,13 @@
+package Management;
+
+public class Rule {
+    String name;
+    String time;
+    String action;
+
+    public Rule(String name, String time, String action) {
+        this.name = name;
+        this.time = time;
+        this.action = action;
+    }
+}
