f7d6a3f8291589c87f08923ac42c79e66df6ec83
Mobin <Mobinmojiri1234@gmail.com>
2025-04-20 18:06:51 +0330

    coding initial of light class

---
+++ src/Management/Light.java
@@ -0,0 +1,22 @@
+package Management;
+
+public class Light extends Device {
+
+    private String status = "off";
+    private int brightness = 50;
+
+    public Light(String name, String protocol) {
+        super(name, protocol);
+    }
+
+    @Override
+    public void setProperty(String property, String value) {
+
+    }
+
+    @Override
+    public String getStatus() {
+        return "";
+    }
+
+}
